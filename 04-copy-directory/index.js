c0ba1a673cd13fe3eb3c1e71d2dc999337ec31ce
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-05-07 10:45:24 +0300

    refactor: refactor 4 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,53 +1,39 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromises = fs.promises;
 
 const sourceFolderPath = path.join(__dirname, './files');
 const destFolderPath = path.join(__dirname, './files-copy');
 
-fs.stat(destFolderPath, (err) => {
-  if (err) {
-    fs.mkdir(destFolderPath, (error) => {
-      if (error) {
-        return process.stdout.write(`Folder creation error: ${error}. Module is stopped\n`);
-      }
-      process.stdout.write(`The folder "${destFolderPath}" was created.\n`);
-      updateDestFolder();
-    });
-  } else updateDestFolder();
-});
+const copyFiles = async () => {
+  try {
+    await fsRm(destFolderPath);
+    await fsPromises.mkdir(destFolderPath, { recursive: true });
+    await fsPromises
+      .readdir(sourceFolderPath, { withFileTypes: true })
+      .then(async (files) => {
+        for (const file of files) {
+          if (file.isFile()) {
+            const sourceFile = path.join(sourceFolderPath, file.name);
+            const destFile = path.join(destFolderPath, file.name);
+            await fsPromises.copyFile(sourceFile, destFile);
+          }
+        }
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const updateDestFolder = () => {
-  fs.readdir(sourceFolderPath, { withFileTypes: true }, (error, data) => {
-    if (error) {
-      return process.stdout.write(`The source folder has an error: ${error}. Module is stopped\n`);
-    }
-    const sourseFiles = data;
-    fs.readdir(destFolderPath, { withFileTypes: true }, (error, data) => {
-      const destFiles = data;
-      destFiles.forEach((destFile) => {
-        // Удаляем файлы, что есть в папке назначения, но нет в исходной папке
-        if (!sourseFiles.find((sorceFile) => sorceFile.name === destFile.name)) {
-          fs.unlink(path.join(destFolderPath, destFile.name), (err) => {
-            if (err) {
-              return process.stdout.write(`File deletion error: ${err}\n`);
-            }
-            process.stdout.write(`The file "${destFile.name}" \t was deleted.\n`);
-          });
-        }
-      });
-      sourseFiles.forEach((sourceFile) => {
-        // Копируем из исходной папки файл, если его нет в папке назначения
-        if (!destFiles.find((destFile) => sourceFile.name === destFile.name)) {
-          const sourceFilePath = path.join(sourceFolderPath, sourceFile.name);
-          const destFilePath = path.join(destFolderPath, sourceFile.name);
-          fs.copyFile(sourceFilePath, destFilePath, (err) => {
-            if (err) {
-              return process.stdout.write(`File copying error: ${err}\n`);
-            }
-            process.stdout.write(`The file "${sourceFile.name}"   \t was copied.\n`);
-          });
-        }
-      });
+const fsRm = (dir) => {
+  return new Promise((resolve, reject) => {
+    fs.rm(dir, { recursive: true }, (err) => {
+      if (err && err.code !== 'ENOENT') {
+        reject(new Error(err));
+      }
+      resolve();
     });
   });
 };
+
+copyFiles();
