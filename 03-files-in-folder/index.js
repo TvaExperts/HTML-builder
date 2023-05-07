df84510961821fe8a4b10ae0540cf554cdc2bc65
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-05-07 13:25:49 +0300

    refactor: refactor 3th task solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,15 +4,20 @@ const path = require('path');
 const folderPath = path.join(__dirname, './secret-folder');
 
 fs.readdir(folderPath, { withFileTypes: true }, (error, data) => {
-  process.stdout.write('================================================== \n');
-  process.stdout.write('Filename \t - \t Ext. \t - \t Size \n');
-  process.stdout.write('================================================== \n');
-
+  if (error) {
+    console.error(new Error(error));
+    return;
+  }
+  console.log('Filename \t - \t Ext. \t - \t Size');
   data.forEach((file) => {
     if (file.isFile()) {
       const filePath = path.join(folderPath, file.name);
       fs.stat(filePath, (error, stats) => {
-        process.stdout.write(`${file.name} \t - \t ${path.extname(file.name)} \t - \t ${(stats.size / 1024).toFixed(2)} kB\n`);
+        if (error) {
+          console.error(new Error(error));
+          return;
+        }
+        console.log(`${file.name} \t - \t ${path.extname(file.name)} \t - \t ${stats.size}`);
       });
     }
   });
