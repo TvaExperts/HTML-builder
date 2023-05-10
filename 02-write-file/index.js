da76196eac986f8ba37de167444ec7b8eb4b9c73
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-05-10 19:27:14 +0300

    fix: fix filerecording (2nd task)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, './text.txt');
-const writeStream = fs.createWriteStream(filePath);
+let writeStream = fs.createWriteStream(filePath);
 
 process.stdout.write('Enter your data!\n');
+
+let enteredData = '';
 
 process.stdin.on('data', (data) => {
   if (data.toString().trim() === 'exit') {
     writeStream.close();
     process.exit();
   }
-  writeStream.write(data);
+  if (writeStream.closed) writeStream = fs.createWriteStream(filePath);
+  enteredData += data;
+  writeStream.write(enteredData);
+  writeStream.close();
 });
 
 process.on('SIGINT', () => process.exit());
